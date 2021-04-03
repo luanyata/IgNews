a4b53c9fc4b8e757940b8d94e91baf71a5aa5c3a
Luan Lima <luan@yata.dev>
2021-04-03 13:13:15 -0300

    :sparkles:  Autenticacao usando github

--- src/components/SingInButton/index.tsx
+++ src/components/SingInButton/index.tsx
@@ -1,19 +1,30 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
 import styles from './styles.module.scss'
-
+import { signIn, signOut, useSession } from 'next-auth/client'
 
 const SignInButton = () => {
-	const isUserLoggedIn = false;
+	const [session] = useSession()
 
-	return isUserLoggedIn ? (
-		<button type="button" className={styles.sigInButton}>
+	console.warn(session);
+
+
+	return session ? (
+		<button
+			type="button"
+			className={styles.sigInButton}
+			onClick={() => signOut()}
+		>
 			<FaGithub color="#04d361" />
-					Luan Yata
+			{session.user.name}
 			<FiX color="#737380" className={styles.closeIcon} />
 		</button >
 	) : (
-		<button type="button" className={styles.sigInButton}>
+		<button
+			type="button"
+			className={styles.sigInButton}
+			onClick={() => signIn('github')}
+		>
 			<FaGithub color="#eba417" />
 				Sign in with Github
 		</button >
