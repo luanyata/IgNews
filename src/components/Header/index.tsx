680637cca78f3471b59ad7aa8b16ae2d51ad890b
Luan Lima <luan@yata.dev>
2021-04-10 20:01:55 -0300

    :sparkles:  Adicao do SPA e Rota Ativa
    Adicao da tag LINK nas ancoras para aproveitamento do core ja baixando segundo o conceito de SPA; Adicao do
    ActiveLink para informar qual link está ativo no menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,20 +1,27 @@
 import SignInButton from '../SingInButton'
 import styles from './styles.module.scss'
+import React from 'react'
+import ActiveLink  from '../ActiveLink'
 
 
 const Header = () => {
-	return (
-		<header className={styles.headerContainer}>
-			<div className={styles.headerContent}>
-				<img src="/images/logo.svg" alt="ig.news" />
-				<nav>
-					<a className={styles.active} href="">Home</a>
-					<a href="">Posts</a>
-				</nav>
-				<SignInButton />
-			</div>
-		</header>
-	)
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <img src="/images/logo.svg" alt="ig.news" />
+        <nav>
+          <ActiveLink activeClassName={styles.active} href="/">
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href="/posts">
+            <a>Posts</a>
+          </ActiveLink>
+
+        </nav>
+        <SignInButton />
+      </div>
+    </header>
+  )
 }
 
 
