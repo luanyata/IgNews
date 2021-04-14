ef1da475c277a9704e861b610c1838ffce6b32f8
Luan Lima <luan@yata.dev>
2021-04-14 19:11:02 -0300

    :sparkles:  Adicao da pagina de preview do post

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -43,7 +43,7 @@ export const getServerSideProps: GetServerSideProps = async ({ req, params }) =>
   console.warn(session);
 
 
-  if (!session.activeSubscription) {
+  if (!session?.activeSubscription) {
     return {
       redirect: {
         destination: '/',
